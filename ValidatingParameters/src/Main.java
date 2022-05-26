f6031164b7cf7a5883e55d7f2051f9298b6ef9cb
kmiller <kmmiller418@gmail.com>
2022-05-26 10:24:43 -0400

    Add ValidatingParameters

---
+++ ValidatingParameters/src/Main.java
@@ -0,0 +1,14 @@
+import validating.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+//        c.factorial(2);
+//        c.factorial(-2);
+//        c.binomialCoefficent(-2,-1);
+//        c.binomialCoefficent(1,2);
+//
+//        Person p = new Person(null,4);
+//        Person p2 = new Person( "george", -1);
+    }
+}
---
+++ ValidatingParameters/src/validating/Calculator.java
@@ -0,0 +1,34 @@
+package validating;
+
+public class Calculator {
+
+    public int factorial(int num) {
+
+        if (num < 0){
+            throw new IllegalArgumentException("Number cannot be less than 0");
+        }
+
+        int answer = 1;
+        for (int i = 1; i <= num; i++) {
+            answer *= i;
+        }
+
+        return answer;
+    }
+
+    public int binomialCoefficent(int setSize, int subsetSize) {
+
+        if (setSize < 0 || subsetSize < 0){
+            throw new IllegalArgumentException("Parameters cannot be negative");
+        }
+
+        if (subsetSize > setSize) {
+            throw new IllegalArgumentException("Subset size cannot exceed set size");
+        }
+
+        int numerator = factorial(setSize);
+        int denominator = factorial(subsetSize) * factorial(setSize - subsetSize);
+
+        return numerator / denominator;
+    }
+}
